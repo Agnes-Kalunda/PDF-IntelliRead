49337dd7880fe5914e2953bd7a0f5e9161db5222
Agnes <agypeter97@gmail.com>
2024-06-18 21:12:13 +0300

    map upload and ask views in urls

--- pdfApp/urls.py
+++ pdfApp/urls.py
@@ -5,9 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
-
+    path('upload/', views.upload_pdf, name='upload_pdf'),
+    path('ask/', views.ask_question, name='ask_question'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
